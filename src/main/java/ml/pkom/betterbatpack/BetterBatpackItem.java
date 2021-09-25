c1e5c0c10a6c5b68ee5b3fea760db27f2b335bd0
PTOM76 <ptms76@outlook.jp>
2021-09-25 21:37:49 +0900

    Port to 1.17.1

--- src/main/java/ml/pkom/betterbatpack/BetterBatpackItem.java
+++ src/main/java/ml/pkom/betterbatpack/BetterBatpackItem.java
@@ -1,11 +1,12 @@
 package ml.pkom.betterbatpack;
 
 import net.minecraft.item.ArmorMaterial;
+import reborncore.common.powerSystem.RcEnergyTier;
 import team.reborn.energy.EnergyTier;
 import techreborn.items.armor.BatpackItem;
 
 public class BetterBatpackItem extends BatpackItem {
-    public BetterBatpackItem(int maxCharge, ArmorMaterial armorMaterial, EnergyTier tier) {
+    public BetterBatpackItem(int maxCharge, ArmorMaterial armorMaterial, RcEnergyTier tier) {
         super(maxCharge, armorMaterial, tier);
     }
 }
